9467030c1d047a7646cf48233fb03dd373fd798b
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2022-11-07 00:15:54 +0100

    input and displays user data

---
+++ inputUserData.py
@@ -0,0 +1,7 @@
+#this program accepts users names , ages and department to display(print)
+
+name=input("What is your name? ")
+age=input("How old are you? ")
+department=input("What department are you in? ")
+
+print("Hello",name,"so you are",age," years old. You are in ",department," department")
